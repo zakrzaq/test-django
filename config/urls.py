837471ff86b26774f70b9ea6de314f493bd6594a
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2024-11-22 21:10:17 +0100

    simple route fix

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse('Hello, world!')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', HttpResponse('Hello, world!')),
+    path('', home),  # Use the view function here
 ]
