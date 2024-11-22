d8f47ca2e8ceef9254a0b4c3d4d6243549ce7077
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2024-11-22 21:27:00 +0100

    static files

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-i82bhlan2g-u#c*_!tdms1i8)(mja^yg1a%ar^ey-9ju-hed*(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["jukesites.com"]
+ALLOWED_HOSTS = ["localhost", "jukesites.com"]
 
 
 # Application definition
@@ -115,6 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
