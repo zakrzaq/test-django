3bb8020e5ebf0934f68e493dc79801c8a01445e6
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2024-11-22 22:00:22 +0100

    test debug off

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i82bhlan2g-u#c*_!tdms1i8)(mja^yg1a%ar^ey-9ju-hed*('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "jukesites.com"]
 
